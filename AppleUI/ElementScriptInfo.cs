efecfa715633102b84a8a9dfa4f98b9ec33ebe8f
qapples <armand.scotty@yahoo.com>
2023-06-16 17:21:42 -0400

    Change order of parameters of ElementScriptInfo

--- AppleUI/ElementScriptInfo.cs
+++ AppleUI/ElementScriptInfo.cs
@@ -7,15 +7,13 @@ namespace AppleUI
     public readonly struct ElementScriptInfo
     {
         public readonly string Name;
-        public readonly Dictionary<string, object> Arguments;
         public readonly bool Enabled;
+        public readonly Dictionary<string, object> Arguments;
         
         [JsonConstructor]
-        public ElementScriptInfo(string name, Dictionary<string, object> arguments, bool enabled)
+        public ElementScriptInfo(string name, bool enabled, Dictionary<string, object> arguments)
         {
-            Name = name;
-            Arguments = arguments;
-            Enabled = enabled;
+            (Name, Enabled, Arguments) = (name, enabled, arguments);
         }
     }
 }
