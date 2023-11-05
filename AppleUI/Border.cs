cdeed8573dfd80ccbc69cd042326c95e6dd54897
qapples <qapples@vt.edu>
2023-11-05 13:42:12 -0500

    Close up gap in border when using Rectangle overload for DrawBorder

--- AppleUI/Border.cs
+++ AppleUI/Border.cs
@@ -30,11 +30,12 @@ namespace AppleUI
         /// <param name="bounds">The <see cref="Rectangle"/> to draw the bounds of. </param>
         public void DrawBorder(SpriteBatch spriteBatch, Rectangle bounds)
         {
+            //Draw order: Left, Right, Top, Bottom
             spriteBatch.Draw(Texture, new Rectangle(bounds.Left, bounds.Top, Thickness, bounds.Height), Color.White);
             spriteBatch.Draw(Texture, new Rectangle(bounds.Right, bounds.Top, Thickness, bounds.Height),
                 Color.White);
             spriteBatch.Draw(Texture, new Rectangle(bounds.Left, bounds.Top, bounds.Width, Thickness), Color.White);
-            spriteBatch.Draw(Texture, new Rectangle(bounds.Left, bounds.Bottom, bounds.Width, Thickness),
+            spriteBatch.Draw(Texture, new Rectangle(bounds.Left, bounds.Bottom, bounds.Width + Thickness, Thickness),
                 Color.White);
         }
         
@@ -66,42 +67,6 @@ namespace AppleUI
             rightRect.Draw(spriteBatch, Texture, Color.White);
             topRect.Draw(spriteBatch, Texture, Color.White);
             bottomRect.Draw(spriteBatch, Texture, Color.White);
-        }
-
-        /// <summary>
-        /// Creates an array of Colors that is representative of the Border struct
-        /// </summary>
-        /// <param name="width">Width of the texture to make a border of</param>
-        /// <param name="height">Height of the texture to make a border of</param>
-        /// <returns>An array of Colors that represents the border</returns>
-        public Color[] CreateBorderColorArray(int width, int height)
-        {
-            Color[] textureColor = new Color[Texture.Width * Texture.Height];
-            Texture.GetData(textureColor);
-            
-            var (outWidth, outHeight) = (width + Thickness, height + Thickness);
-            
-            Color[] outArray = new Color[outWidth * outHeight];
-            
-            int texIndex = 0;
-            for (int r = 0; r < outHeight; r++)
-            {
-                for (int c = 0; c < outWidth; c++)
-                {
-                    //if both row and column are past the border, then make it transparent to allow for something else  
-                    //to be displayed.otherwise, then set it to a pixel from the border texture
-                    if (r >= Thickness && c >= Thickness && r < height && c < width)
-                    {
-                        outArray[(r * outHeight) + c] = Color.Transparent;
-                    }
-                    else
-                    {
-                        outArray[(r * outHeight) + c] = textureColor[texIndex++ % textureColor.Length];
-                    }
-                }
-            }
-
-            return outArray;
         }
     }
 }
